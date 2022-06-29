c307ca81299164a3b1c46674146abfda327a2660
Andrei <iandy1731@gmail.com>
2022-06-29 18:37:04 +0300

    123

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 /* ---SIDE-MENU--- */
 
 document.querySelector('.burger').addEventListener('click', function (event) {
@@ -19,5 +18,49 @@ document.documentElement.addEventListener("click", function (e) {
 })
 
 
+/* --- BLOCK ANIMATION --- */
+const animItems = document.querySelectorAll("._anim-items");
+
+if (animItems.length > 0) {
+    window.addEventListener("scroll", animOnScroll);
+    function animOnScroll() {
+        for (let i = 0; i < animItems.length; ++i) {
+            const animItem = animItems[i];
+            const animItemHeigth = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 3;
+            const animItemPoint = (animItemHeigth > window.innerHeight) ?
+                window.innerHeight - window.innerHeight / animStart
+                :
+                window.innerHeight - animItemHeigth / animStart;
 
 
+            if ((pageYOffset > animItemOffset - animItemPoint && pageYOffset < (animItemOffset + animItemHeigth))) {
+                animItem.classList.add(`_active`);
+            } else if (animItem.classList.contains("_anim-repeat")) {
+                animItem.classList.remove(`_active`);
+            }
+        }
+    }
+
+    function offset(element) {
+        const rect = element.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+    setTimeout(() => {
+        animOnScroll();
+    }, 300);
+}
+
+/* --- FAKE ROUTING --- */
+const links = Array.from(document.body.getElementsByTagName(`A`));
+links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+        if (!e.target.classList.contains("footer__email")) {
+            alert("Fake routing");
+            e.preventDefault();
+        }
+    })
+})
